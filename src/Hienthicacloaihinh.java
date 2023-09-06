6521412e91c363c7e50920913f6c3f302b92151f
BuiLongCuong <builongcuong76@gmail.com>
2023-09-06 11:52:36 +0700

    demo bài tập vòng lặp

---
+++ src/Hienthicacloaihinh.java
@@ -0,0 +1,10 @@
+public class Hienthicacloaihinh {
+    public static void main(String[] args) {
+        for (int i = 1; i <=3; i++) {
+            System.out.println();
+            for (int j = 1; j <=7; j++) {
+                System.out.print("* ");
+            }
+        }
+    }
+}
