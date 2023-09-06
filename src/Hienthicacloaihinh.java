7917b32d2e8e82b7ba0d2ca54edfcbc2af7dc6bd
BuiLongCuong <builongcuong76@gmail.com>
2023-09-06 14:48:16 +0700

    demo

--- src/Hienthicacloaihinh.java
+++ src/Hienthicacloaihinh.java
@@ -1,10 +1,26 @@
 public class Hienthicacloaihinh {
     public static void main(String[] args) {
-        for (int i = 1; i <=3; i++) {
-            System.out.println();
-            for (int j = 1; j <=7; j++) {
-                System.out.print("* ");
-            }
-        }
+
+//        for (int i = 1; i <=3; i++) {
+//            System.out.println();
+//            for (int j = 1; j <=7; j++) {
+//                System.out.print("* ");
+//            }
+//        }
+
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("");
+//            for (int j = 1; j <=i; j++) {
+//                System.out.print("* ");
+//            }
+//        }
+
+//        for (int i = 5; i >=1; i--) {
+//            System.out.println();
+//            for (int j = 1; j <=i ; j++) {
+//                System.out.print("* ");
+//            }
+//        }
+
     }
 }
