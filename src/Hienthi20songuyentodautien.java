92aa98ecdeea615930eb9cd9ebc1997c80819155
BuiLongCuong <builongcuong76@gmail.com>
2023-09-06 14:01:41 +0700

    demo

---
+++ src/Hienthi20songuyentodautien.java
@@ -0,0 +1,20 @@
+public class Hienthi20songuyentodautien {
+    public static void main(String[] args) {
+       int a=1;
+       int countSNT=0;
+       while (true){
+           int countUoc=0;
+           for (int i = 1; i <=a; i++) {
+               if(a%i==0){
+                   countUoc++;
+               }
+           }if(countUoc==2){
+               countSNT++;
+               System.out.println(a);
+           }a++;
+           if(countSNT==20){
+               break;
+           }
+       }
+    }
+}
